9da495094ae7c17252ad1a70e0b1c975dff73d06
NavKalaeen <jammalamadugunaveen@gmail.com>
2024-08-18 09:57:26 +0530

    success

---
+++ Happy Independence/script.js
@@ -0,0 +1,194 @@
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('sing');
+    val.style.display='none';
+    v1.style.display='block';
+},3000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('sing');
+    v1.style.display='none';
+    val.style.display='block';
+},6000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('bose');
+    val.style.display='none';
+    v1.style.display='block';
+},9000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('bose');
+    v1.style.display='none';
+    val.style.display='block';
+},12000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('gandhi');
+    val.style.display='none';
+    v1.style.display='block';
+},15000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('gandhi');
+    v1.style.display='none';
+    val.style.display='block';
+},18000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('nehru');
+    val.style.display='none';
+    v1.style.display='block';
+},21000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('nehru');
+    v1.style.display='none';
+    val.style.display='block';
+},24000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('alluri');
+    val.style.display='none';
+    v1.style.display='block';
+},27000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('alluri');
+    v1.style.display='none';
+    val.style.display='block';
+},30000);
+
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('rani');
+    val.style.display='none';
+    v1.style.display='block';
+},33000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('rani');
+    v1.style.display='none';
+    val.style.display='block';
+},36000);
+
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('sardar');
+    val.style.display='none';
+    v1.style.display='block';
+},39000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('sardar');
+    v1.style.display='none';
+    val.style.display='block';
+},42000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('azad');
+    val.style.display='none';
+    v1.style.display='block';
+},45000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('azad');
+    v1.style.display='none';
+    val.style.display='block';
+},48000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('naidu');
+    val.style.display='none';
+    v1.style.display='block';
+},51000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('naidu');
+    v1.style.display='none';
+    val.style.display='block';
+},54000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('lal');
+    val.style.display='none';
+    v1.style.display='block';
+},57000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('lal');
+    v1.style.display='none';
+    val.style.display='block';
+},60000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('lajpat');
+    val.style.display='none';
+    v1.style.display='block';
+},63000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('lajpat');
+    v1.style.display='none';
+    val.style.display='block';
+},66000);
+
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('bala');
+    val.style.display='none';
+    v1.style.display='block';
+},69000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('bala');
+    v1.style.display='none';
+    val.style.display='block';
+},72000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('happy');
+    val.style.display='none';
+    v1.style.display='block';
+},75000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('happy');
+    v1.style.display='none';
+    val.style.display='block';
+},78000);
+
+setTimeout(()=>{
+    var val = document.getElementById('chakra');
+    var v1 = document.getElementById('proud');
+    val.style.display='none';
+    v1.style.display='block';
+},78000);
+
+
